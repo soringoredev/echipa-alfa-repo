ea025c97673e3b2467ef5fb13e26a9a23b7a6805
Sorin Gore <soringoredev@gmail.com>
2025-10-28 18:57:06 +0200

    Add Sorin Gore 2

--- src/Ex1.java
+++ src/Ex1.java
@@ -9,6 +9,6 @@ public class Ex1 {
         System.out.println("Victor Lungu 2");
         System.out.println("Ion Schimbator 2");
         System.out.println("Vasile Pusca 2");
-
+        System.out.println("Sorin Gore 2");
     }
 }
